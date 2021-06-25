9e508fa48d059ffca5424a6db3d8180b2450ada8
Has <>
2021-06-26 00:10:25 +0900

    [ADD] vue-sfc-rollup init

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { TsClassEnumSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  TsClassEnumSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <ts-class-enum-sample />
+  </div>
+</template>
---
+++ ts-class-enum.d.ts
@@ -0,0 +1,7 @@
+import Vue, { PluginFunction, VueConstructor } from 'vue';
+
+
+declare const TsClassEnum: PluginFunction<any>;
+export default TsClassEnum;
+
+export const TsClassEnumSample: VueConstructor<Vue>;
